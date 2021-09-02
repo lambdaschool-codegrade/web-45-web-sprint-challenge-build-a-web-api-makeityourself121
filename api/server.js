1a4236e76cd1671d7396b40bff802db3a0a9d68c
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-02 13:26:24 -0700

    basic server running

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const server = express();
+const express = require('express')
+const server = express()
+const projectsRouter = require('./projects/projects-router')
+server.use(express.json())
 
+server.use('/api/projects', projectsRouter)
+
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: 'Not Found',
+  })
+})
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
-module.exports = server;
+module.exports = server

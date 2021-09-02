c705dbddee2270ae0eed14488041366a3b993b4f
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-02 14:53:19 -0700

    some tests passing

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,42 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+
+function validateUserId(req, res, next) {
+  const { id } = req.params
+  Projects.get(id).then((project) => {
+    if (project) {
+      req.project = project
+      next()
+    } else {
+      res.status(404).json({
+        message: 'user not found',
+      })
+    }
+  })
+}
+
+function validatePost(req, res, next) {
+  const { name, description } = req.body
+
+  if (!name || !description) {
+    res.status(400).json({
+      message: 'missing required text field',
+    })
+  } else {
+    next()
+  }
+}
+
+function validatePostCompleted(req, res, next) {
+  const { name, description, completed } = req.body
+
+  if (!name || !description || completed == undefined) {
+    res.status(400).json({
+      message: 'missing required text field',
+    })
+  } else {
+    next()
+  }
+}
+
+module.exports = { validateUserId, validatePost, validatePostCompleted }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,12 +1,68 @@
 // Write your "projects" router here
 const express = require('express')
 const router = express.Router()
+const Projects = require('./projects-model')
+const {
+  validateUserId,
+  validatePost,
+  validatePostCompleted,
+} = require('./projects-middleware')
+router.get('/', (req, res, next) => {
+  Projects.get(req.params.id)
+    .then((project) => {
+      res.status(200).json(project)
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
 
-router.get('/', (req, res) => {})
-router.get('/:id', (req, res) => {})
-router.post('/', (req, res) => {})
-router.put('/:id', (req, res) => {})
-router.delete('/:id', (req, res) => {})
+router.get('/:id', validateUserId, (req, res) => {
+  res.json(req.project)
+})
+
+router.post('/', validatePost, (req, res, next) => {
+  Projects.insert(req.body)
+    .then((project) => {
+      res.status(201).json(project)
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+router.put(
+  '/:id',
+  validateUserId,
+  validatePostCompleted,
+  async (req, res, next) => {
+    try {
+      await Projects.update(req.params.id, req.body)
+      res.status(200).json(req.body)
+    } catch (err) {
+      next(err)
+    }
+  },
+)
+
+router.delete('/:id', validateUserId, (req, res, next) => {
+  Projects.get(req.params.id)
+    .then((project) => {
+      Projects.remove(req.params.id).then(() => {
+        res.status(200).json(project)
+      })
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
 router.get('/:id/actions', (req, res) => {})
 
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: 'something is wrong with the server',
+    message: err.message,
+  })
+})
 module.exports = router
